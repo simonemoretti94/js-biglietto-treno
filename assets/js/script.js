1c701c74227a9b3773e132c418e9774df20cd14c
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-17 15:30:20 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+//script
+
+// const x = prompt("glad to see you again, please write a number"); 
+
+// console.log("elaborating an output, pls wait...");
+
+// let output;
+
+// if (x > 4) {
+//     output = `your number ${x}  is greater than four.`;
+// }
+// else if (x < 4) {
+//     output = `your number ${x} is lower than four.`;
+// }
+// else {
+//     output = `your number ${x} is exactly what i was looking for.`;
+// }
+
+// console.log(output);
+
+// let avviso;
+
+// const nome = prompt('inserisci il tuo nome');
+
+// if (nome.length < 6) {
+//     alert('non sei simone');
+// }
+// else if (nome.length > 6) {
+//     alert('non sei simone');
+// }
+// else {
+
+//     alert(`quindi sei ${nome}`);
+//     avviso =`sei ${nome}`;
+
+    
+
+//     if (avviso == 'sei simone') {
+//         console.log('benvenuto');
+//     }
+//     else {
+//         console.log('acesso negato');
+//     }
+// }
