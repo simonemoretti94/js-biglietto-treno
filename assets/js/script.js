c79541c5eea398977de2c7ad41167217a0a46e2d
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-17 15:45:44 +0100

    second

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,36 @@
 //         console.log('acesso negato');
 //     }
 // }
+
+
+// asking n kilometers
+const km = prompt('please write below the amount of km you\'re gonna grind :)');
+
+// asking a age
+const age = prompt('then, please write below your age');
+
+// information for travellers
+alert("hi, i'm here just to help and let you know some infos: 1 - you're paying 0.21€ each km. 2 - 20% discount is applied on teen below 18 years old 3 - 40% discount is applied on elderly over 65 years old.");
+
+let price;
+let tot_price;
+
+price = km * 0.21;
+
+//output price without discounts
+console.log(`${price}€`);
+
+//getting td km
+const kmtd =document.getElementById('km');
+
+//writing on td km
+kmtd.innerText = `${km}`;
+
+//getting td age
+const agetd = document.getElementById('age');
+
+//writing on td age
+agetd.innerText = `${age}`;
+
+// checking for discount rights
+
