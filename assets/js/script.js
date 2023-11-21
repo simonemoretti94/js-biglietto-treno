becb79f0dbad904f8e9f74aee0064533cfdab745
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-21 14:02:18 +0100

    6th fixed media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,5 @@
 //script
 
-// const x = prompt("glad to see you again, please write a number"); 
-
-// console.log("elaborating an output, pls wait...");
-
-// let output;
-
-// if (x > 4) {
-//     output = `your number ${x}  is greater than four.`;
-// }
-// else if (x < 4) {
-//     output = `your number ${x} is lower than four.`;
-// }
-// else {
-//     output = `your number ${x} is exactly what i was looking for.`;
-// }
-
-// console.log(output);
-
-// let avviso;
-
-// const nome = prompt('inserisci il tuo nome');
-
-// if (nome.length < 6) {
-//     alert('non sei simone');
-// }
-// else if (nome.length > 6) {
-//     alert('non sei simone');
-// }
-// else {
-
-//     alert(`quindi sei ${nome}`);
-//     avviso =`sei ${nome}`;
-
-    
-
-//     if (avviso == 'sei simone') {
-//         console.log('benvenuto');
-//     }
-//     else {
-//         console.log('acesso negato');
-//     }
-// }
-
-
 // asking n kilometers
 const km = prompt('please write below the amount of km you\'re gonna grind :)');
 
